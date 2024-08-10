144f1b0385086750f4bab959b98d4714532beb08
hanumantha <hanumanth.ht461@gmail.com>
2024-08-10 19:45:28 +0530

    instlled material ui framework

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-     Welcome page
-     hello
+     Welcome to My website
     </main>
   );
 }
